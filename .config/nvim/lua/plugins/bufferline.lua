95bf03b01a11eeee999c893ae823dc3242ee4e88
Jason Biggs <jason.biggs@ext.chime.com>
2025-02-12 09:06:21 -0600

    added bufferline keys and updated trouble keys

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -10,5 +10,9 @@ return {
         separator_style = "thick",
       },
     })
+    vim.keymap.set("n", "<leader>bl", "<cmd>BufferLineCycleNext<CR>", {desc = "Next buffer"})
+    vim.keymap.set("n", "<leader>bh", "<cmd>BufferLineCyclePrev<CR>", {desc = "Previous buffer"})
+    vim.keymap.set("n", "<leader>bc", "<cmd>BufferLinePickClose<CR>", {desc = "Close buffer"})
+    vim.keymap.set("n", "<leader>bf", "<cmd>BufferLinePick<CR>", {desc = "Find buffer"})
   end,
 }
