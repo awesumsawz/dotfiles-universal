5731299743a94ee268099349bc8539f56e0d3fc4
Jason Biggs <jason.biggs@ext.chime.com>
2025-02-12 08:38:04 -0600

    updated treesitter so that it's working as expected again

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,49 +1,44 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-		config = function()
-			local configs = require("nvim-treesitter.configs")
-      vim.notify = require("notify")
-			configs.setup({
-				ensure_installed = { 
-					"javascript", 
-					"typescript", 
-					"php", 
-					"lua", 
-					"markdown", 
-					"bash", 
-					"css", 
-					"html", 
-					"phpdoc", 
-					"python", 
-					"regex", 
-					"scss", 
-					"sql", 
-					"yaml",
-				},
-				sync_install = false,
-				auto_install = true,
-				highlight = { 
-					enable = true 
-				},
-				indent = { 
-					enable = true 
-				},  
-			})
-		end,
-		
-		-- Custom parser configuration for Blade
-		config = function()
-			local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
-			parser_config.blade = {
-				install_info = {
-					url = "https://github.com/EmranMR/tree-sitter-blade",
-        files = { "src/parser.c" },
-					branch = "main",
-				},
-				filetype = "blade",
-			}
-		end,
-	}
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      local configs = require("nvim-treesitter.configs")
+      configs.setup({
+        ensure_installed = {
+          "javascript",
+          "typescript",
+          "php",
+          "lua",
+          "markdown",
+          "bash",
+          "css",
+          "html",
+          "phpdoc",
+          "python",
+          "regex",
+          "scss",
+          "sql",
+          "yaml",
+        },
+        sync_install = false,
+        auto_install = true,
+        highlight = {
+          enable = true
+        },
+        indent = {
+          enable = true
+        },
+      })
+      local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+      parser_config.blade = {
+        install_info = {
+          url = "https://github.com/EmranMR/tree-sitter-blade",
+          files = { "src/parser.c" },
+          branch = "main",
+        },
+        filetype = "blade",
+      }
+    end,
+  }
 }
