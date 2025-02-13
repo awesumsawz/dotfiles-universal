f3aa1645f18ca88bca6d096e9470f3b088964c93
Jason Biggs <development@jason-biggs.com>
2025-02-12 21:34:30 -0600

    removed telescope keymaps (replaced by fzf)

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,14 +1,12 @@
 -- plugins/telescope.lua:
 return {
   {
-    'nvim-telescope/telescope.nvim', 
+    'nvim-telescope/telescope.nvim',
     tag = '0.1.8',
-    dependencies = { 
-      'nvim-lua/plenary.nvim' 
+    dependencies = {
+      'nvim-lua/plenary.nvim'
     },
     keys = {
-      { "<leader>pf", "<cmd>:Telescope find_files<CR>", desc = "Project Find" },
-      { "<leader>pg", "<cmd>:Telescope live_grep<CR>", desc = "Project Grep" },
     }
   },
   {
