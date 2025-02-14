8890d7815e242c3fb50447bdebcf0b89bc54a10c
Jason Biggs <jason.biggs@ext.chime.com>
2025-02-14 10:18:17 -0600

    styling eza

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -15,8 +15,8 @@ alias untarball="tar -xzvf"
 # client shortcuts
 alias modelhorseuniversity="cd ~/code/clients/MHU/websites/model-horse-university/"
 # eza
-alias ll="eza --color=always --long --git --icons=always --all"
-alias ls="eza --color=always --git --icons=always --all"
+alias ll="eza --color=always --color=auto --long --git --icons=always --all"
+alias ls="eza --color=always --color=auto --git --icons=always --all"
 # lazygit
 alias lg="lazygit"
 # zoxide
