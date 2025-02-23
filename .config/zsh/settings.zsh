3a4f69493967f688405a20b0c6430494c7467523
Jason Biggs <jason@thinkbigg.dev>
2025-02-23 12:21:58 -0600

    setting zsh formatting

--- .config/zsh/settings.zsh
+++ .config/zsh/settings.zsh
@@ -1,22 +1,22 @@
 # ==================================================
-# ZSH SETTINGS
-# ----START-----------------------------------------
-# history
+# ZSH HISTORY SETTINGS
+# --------------------------------------------------
+HISTFILE=$HOME/.zsh_history
 HISTSIZE=10000
 SAVEHIST=10000
-HISTFILE=~/.zsh_history
 HISTDUP=erase
-setopt appendhistory
-setopt sharehistory
-setopt hist_ignore_space
-setopt hist_ignore_all_dups
-setopt hist_save_no_dups
-setopt hist_ignore_dups
-setopt hist_find_no_dups
-# editor
-if [[ -n $SSH_CONNECTION ]]; then
-  export EDITOR='vim'
-else
-  export EDITOR='nvim'
-fi
-# ----END-------------------------------------------
+
+# History Options
+setopt appendhistory     # Append to history file
+setopt sharehistory      # Share history across sessions
+setopt hist_ignore_space # Don't record commands starting with space
+setopt hist_ignore_all_dups # Remove older duplicate entries
+setopt hist_save_no_dups # Don't save duplicate entries
+setopt hist_ignore_dups  # Don't record duplicates
+setopt hist_find_no_dups # Don't display duplicates when searching
+
+# ==================================================
+# EDITOR SETTINGS
+# --------------------------------------------------
+# Use neovim by default, fallback to vim when in SSH
+export EDITOR="${SSH_CONNECTION:+vim}${SSH_CONNECTION:-nvim}"
