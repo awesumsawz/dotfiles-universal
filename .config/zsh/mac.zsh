8ed4b6862e5948f643482ddc373f73b455ca420b
Jason Biggs <jason@thinkbigg.dev>
2025-03-01 22:56:26 -0600

    moved github and homebrew exports and evals to mac specific zsh

--- .config/zsh/mac.zsh
+++ .config/zsh/mac.zsh
@@ -4,6 +4,8 @@
 if [[ -f "/opt/homebrew/bin/brew" ]]; then
     eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
+export HOMEBREW_BREWFILE="$HOME/.config/brewfile/brewfile"
+export DYLD_FALLBACK_LIBRARY_PATH="$(brew --prefix)/lib:$DYLD_FALLBACK_LIBRARY_PATH"
 
 # Path configurations
 export PATH="${HOME}/.config/herd-lite/bin:${PATH}"
@@ -15,3 +17,6 @@ export PHP_INI_SCAN_DIR="${HOME}/.config/herd-lite/bin:${PHP_INI_SCAN_DIR}"
 # Nix-Darwin aliases
 unalias drs 2>/dev/null  # Suppress error if alias doesn't exist
 alias drs="darwin-rebuild switch --flake ~/nix#knight"
+
+# github 
+eval "$(gh copilot alias -- zsh)"
