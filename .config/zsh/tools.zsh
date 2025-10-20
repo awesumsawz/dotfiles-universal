89b22befa700d007943df3be466319805bb5b272
Jason Biggs <development@jason-biggs.com>
2025-10-20 09:48:25 -0500

    commented out 1password from zshrc

--- .config/zsh/tools.zsh
+++ .config/zsh/tools.zsh
@@ -4,23 +4,30 @@ echo "Loading tools zsh..."
 # ----START-----------------------------------------
 # fzf
 eval "$(fzf --zsh)"
+
 # ghostty
 export GHOSTTY_CONFIG="$HOME/.config/ghostty/config"
+
 # starship
 export STARSHIP_CONFIG="$HOME/.config/starship/starship.toml"
 eval "$(starship init zsh)" 
+
 # zoxide
 eval "$(zoxide init zsh)"
+
 # thefuck
 eval "$(thefuck --alias fix)"
+
 # eza
 export EZA_CONFIG_DIR="$HOME/.config/eza"
 export LS_COLORS="$(vivid generate dracula)"
-# 1password
-if [[ -f ~/.op_session ]]; then
-    source ~/.op_session
-fi
-eval "$(op signin)" > ~/.op_session
+
+# # 1password
+# if [[ -f ~/.op_session ]]; then
+#     source ~/.op_session
+# fi
+# eval "$(op signin)" > ~/.op_session
+
 # bun
 export BUN_INSTALL="$HOME/.bun"
 export PATH="$BUN_INSTALL/bin:$PATH"
